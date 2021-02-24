95518675a3c84298c873a3464771277bd23ed236
manami-d <manami.davidson@hotmail.com>
2021-02-24 15:37:22 +1100

    Complete with weather card on js file

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // require('dotenv').config();
 
-const card = document.querySelector('.card-body');
-const button = document.querySelector('.btn');
-card.hidden = true;
-
 const emojis = {
   "01d": "☀️",
   "02d": "⛅️",
@@ -25,16 +21,42 @@ const emojis = {
   "50n": "💨",
 };
 
+const htmlCard = (cityName, weatherEmoji, temp, feelsLike) => { 
+  const html = `
+  <div class="container d-flex justify-content-center">
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+         <h4>${`Weather in ${cityName} is ${weatherEmoji}`}</h4>
+         <h5>${`${temp} degree`}</h5>
+         <p>${`feels like: ${feelsLike} degree`}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  `;
+  return html;
+};
+
+//select everything needed
+const card = document.querySelector('.card-body');
+const button = document.querySelector('.btn');
+const weatherCard = document.querySelector('.weatherCard')
+
 button.addEventListener('click', async (event) => {
   event.preventDefault()
-  let city = document.querySelector('input').value.toLowerCase();
+  let inputCity = document.querySelector('input').value.toLowerCase();
   // console.log(city);
-  // const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}`);
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${your apiKey here}`);
-
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=d1f683e5ef0e53192c200bcb85773a55`);
   const data = await response.json();
-  card.hidden = false;
-  card.innerHTML =`Weather in ${data.name} is ${emojis[data.weather[0].icon]}, ${Math.round(data.main.temp -273.15)} degree`;
+  const cityName = data.name;
+  console.log(data);
+  const weatherEmoji = emojis[data.weather[0].icon];
+  const temp = Math.round(data.main.temp -273.15);
+  const feelsLike = Math.round(data.main.feels_like -273.15); 
+  weatherCard.innerHTML = htmlCard(cityName, weatherEmoji, temp, feelsLike) ;
   // console.log(data)
 })
   
